d321515b9b7a1321114f80fc769c924a4ac235f6
Tomas <tomas.millan96@gmail.com>
2022-10-31 17:48:15 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+import path, { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { PORT } from "./config.js";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
+
+app.use(express.static(join(__dirname, "../client/build")));
+
+server.listen(PORT);
+console.log(`Server has started on ${PORT}`);
